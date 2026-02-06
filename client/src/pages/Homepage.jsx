7b42f178c31d33c61839967186f59feb8933e5f4
sahil <deepsahil.online@gmail.com>
2026-02-06 18:31:24 +0530

    added scroll parallax to hero section

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,35 +1,64 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import HeroSection from "../components/HeroSection";
 import ProjectsSection from "../components/ProjectSection";
 import SkillSection from "../components/SkillSection";
 import AboutSection from "../components/AboutSection";
+import Footer from "../components/Footer";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Footer from "../components/Footer";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Homepage = () => {
+  const heroRef = useRef(null);
+
+  useEffect(() => {
+    // Simple parallax effect on hero section
+    gsap.to(heroRef.current, {
+      y: 250, // Reduced movement for subtler effect
+      ease: "none",
+      scrollTrigger: {
+        trigger: heroRef.current,
+        start: "top top",
+        end: "bottom top",
+        scrub: 0.5, // Faster scrubbing for smoother feel
+        invalidateOnRefresh: true,
+      },
+    });
+
+    // Cleanup
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
   return (
     <>
-      <div className="bg-zinc-200 w-full overflow-hidden">
-
-        <section id="home">
+      <div className="bg-zinc-200 w-full overflow-hidden relative">
+        {/* Hero Section with parallax */}
+        <section 
+          id="home" 
+          ref={heroRef} 
+          className="relative z-0"
+          style={{ willChange: 'transform' }}
+        >
           <HeroSection />
         </section>
 
-        <section id="work">
-          <ProjectsSection />
-        </section>
+        {/* All other sections scroll normally over the hero */}
+        <div className="relative z-10 bg-zinc-200">
+          <section id="work">
+            <ProjectsSection />
+          </section>
 
-        <section id="about">
-          <AboutSection />
-        </section>
+          <section id="about">
+            <AboutSection />
+          </section>
 
           <SkillSection />
 
-        {/* <Footer /> */}
-
+          {/* <Footer /> */}
+        </div>
       </div>
     </>
   );
